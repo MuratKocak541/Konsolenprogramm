3e0b5a8fa0b5af919f853bff8c5b5f1dbe93ff7e
MuratKocak541 <murat.kocal1@icloud.com>
2025-03-18 09:27:35 +0100

    Bonusklassen

---
+++ Bonusklassen.dart
@@ -0,0 +1,13 @@
+class Pet {
+// Attribute
+String type;
+String name; int age;
+// Konstruktor
+Pet(this. type, this.name, this.age);
+}
+void main() {
+Pet pet1 = Pet("Dog", "Buddy", 3);
+Pet pet2 = Pet( "Cat", "Luna", 2);
+Pet myFavPet = Pet( "Dog", "Mable"
+, 4) ;
+}
